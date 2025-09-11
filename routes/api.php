64c4a1a2a1db2bca1c6a7ca635849571be50e6dd
Valdir Júnior <valdirmsjunior@gmail.com>
2025-09-11 16:43:00 -0300

    retirar comentarios

--- routes/api.php
+++ routes/api.php
@@ -8,13 +8,8 @@ use App\Http\Controllers\Api\AuthorController;
 use App\Http\Controllers\Api\MemberController;
 use App\Http\Controllers\Api\BorrowingController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-//Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
